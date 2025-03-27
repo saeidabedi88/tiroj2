814e986ee2f207d4acf2b9223c062ce2b907b7da
saeidabedi88 <saeidabedi88@gmail.com>
2025-03-27 13:57:01 -0400

    Add deployment configuration files

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+        name: 'tirojnet',
+        script: 'npm',
+        args: 'start',
+        env: {
+            NODE_ENV: 'production',
+            PORT: 3000
+        },
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }]
+}; 
